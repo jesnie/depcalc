e301ee48c6b840967e7075e71c485a1021e6c226
Jesper Nielsen <jespernielsen1982@gmail.com>
2023-08-16 09:48:17 +0200

    Add github action to keep requirements up-to-date.

---
+++ compreq.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+set -e
+
+git fetch origin main
+git checkout main
+git pull --rebase origin main
+if ! git checkout -b compreq; then
+    git branch -D compreq
+    git checkout -b compreq
+fi
+poetry run python -m requirements
+if [[ $(git status --porcelain) ]]; then
+    poetry update
+    git \
+        -c "user.name=Update requirements bot" \
+        -c "user.email=none" \
+        commit \
+        -am "Update requirements."
+    git push origin +compreq
+    gh pr create \
+       --title "Update requirements" \
+       --body "Automatic update of requirements."
+       --reviewer jesnie
+    false
+fi
